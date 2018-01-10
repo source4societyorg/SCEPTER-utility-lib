1ea48a9a34b8d506f7c9c7aa2d92855c15d954d3
Daniel Fredriksen <dfredriksen@cyint.technology>
2018-01-10 03:27:47 -0800

    Added common patterns for services

--- utilities.js
+++ utilities.js
@@ -1,5 +1,20 @@
 const utilities = {
-  isEmpty: (value) => (typeof value === 'undefined' || value === null || value === '' || value.length === 0 || value === {} || value === 'NaN')
+  isEmpty: (value) => (typeof value === 'undefined' || value === null || value === '' || value.length === 0 || (typeof value === 'object' && Object.keys(value).length < 1) || value === 'NaN'),
+  standardErrorHandler: (callback, service) => (err) => {
+    let response = service.prepareErrorResponse(err)
+    callback(null, response)
+  },
+  standardSuccessHandler: (callback, service) => (responseData) => {
+    let response = service.prepareSuccessResponse(responseData)
+    callback(null, response)
+  },
+  standardCallbackHandler: (err, data, onErrorCallback, onSuccessCallback) => {
+    if (utilities.isEmpty(err)) {
+      onSuccessCallback(data)
+    } else {
+      onErrorCallback(err)
+    }
+  }
 }
 
 module.exports = utilities
