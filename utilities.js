283aaad82b43a3a13139fa334feb46c3dff40c79
Daniel Fredriksen <dfredriksen@cyint.technology>
2018-01-13 23:41:57 -0800

    Added random key generator

--- utilities.js
+++ utilities.js
@@ -14,6 +14,14 @@ const utilities = {
     } else {
       onErrorCallback(err)
     }
+  },
+  keyGenerator: (length) => {  
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    let retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
   }
 }
 
